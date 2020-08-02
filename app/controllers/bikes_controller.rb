6d33ca37df4bc73f2ce8994d4fa14acb50d65ba3
Alexandre Kantjas <alexandre.kantjas@gmail.com>
2020-08-02 21:44:38 +0200

    Bike Edit / Update

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -4,10 +4,32 @@ class BikesController < ApplicationController
     end
 
     def show
-        @bike = Bike.find_by(params[:id])
+        @bike = Bike.find(params[:id])
     end
 
     def new
         @bike = Bike.new
+    end
+
+    def create
+        @bike = Bike.new(bike_params)
+        @bike.save
+        redirect_to bike_path(@bike)
+    end
+
+    def edit
+        @bike = Bike.find(params[:id])
+    end
+
+    def update
+        @bike = Bike.find(params[:id])
+        @bike.update(bike_params)
+        redirect_to bike_path(@bike)
+    end
+
+    private
+
+    def bike_params
+        params.require(:bike).permit(:name, :rating, :price, :vendor)
     end
 end
