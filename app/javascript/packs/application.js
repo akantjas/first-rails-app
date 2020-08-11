605e9d5937a36762f705feec5f1019d80db93eba
Alexandre Kantjas <alexandre.kantjas@gmail.com>
2020-08-11 19:25:34 +0200

    Simple form added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
 
-
+import "bootstrap";
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -15,4 +15,4 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log("Hello from app/javascript/packs/application.js!");
+
